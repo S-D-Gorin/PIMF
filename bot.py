aab1450a34117aaae58cb42d718aff7e9e619835
S.D.Gorin <s.d.gorin@gmail.com>
2023-10-22 15:59:27 +0300

    init

---
+++ bot.py
@@ -0,0 +1,73 @@
+import telebot
+import re
+import settings
+
+
+BOT_TOKEN = settings.BOT_TOKEN
+GROUP_ID = settings.GROUP_ID
+bot = telebot.TeleBot(BOT_TOKEN)
+DATABASES = {
+    'bot': 'BM.txt',
+    'correct': 'CM.txt',
+    'uncorrect': 'UM.txt'
+}
+
+def db_write(db_name, mode, data):
+    with open(db_name, mode=mode, encoding='utf-8') as db:
+        db.write(data)
+
+
+
+
+
+
+
+@bot.message_handler(content_types = ['text'])
+def find_phone_number(message):
+    
+    
+    
+    if re.search(r'(\+7|8|7).*?(\d{3}).*?(\d{3}).*?(\d{2}).*?(\d{2})', message.text):
+        correct_message = f'correct --- {message.message_id} --- {message.from_user.username} --- {message.from_user.first_name} --- {message.text} \n'
+        db_write(DATABASES.get('correct'), 'a', correct_message)
+    
+    
+    
+    else:
+        if str(message.from_user.username) in settings.SUPERUSERS:
+            pass
+        
+        else:
+            uncorrect_message = f'uncorrect --- {message.message_id} --- {message.from_user.username} --- {message.from_user.first_name} --- {message.text} \n'
+            db_write(DATABASES.get('uncorrect'), 'a', uncorrect_message)
+
+            bot.delete_message(message.chat.id, message.message_id)
+
+            if message.from_user.username == None:
+                user = message.from_user.first_name
+            else:
+                user = f'@{message.from_user.username}'
+                
+
+            message_id_to_del = bot.send_message(
+                message.chat.id, 
+                f'{user} - ваше сообщение удалено. \n ▪️Чтобы отправить сообщение, добавьте номер телефона в объявление (Начиная с +7 или 8)',
+                protect_content=True, 
+                disable_notification=True
+                ).message_id
+            
+            
+            db_write(DATABASES.get('bot'), 'a', f'{str(message_id_to_del)}\n')
+            
+            with open(DATABASES.get('bot'), 'r', encoding='utf-8') as bot_message_id:
+                message_id = bot_message_id.readlines()[-2]
+                bot.delete_message(message.chat.id, int(message_id))
+                if len(bot_message_id.readlines()) >= 10:
+                    db_write(DATABASES.get('bot'), 'w', str(message_id))
+
+
+
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
---
+++ settings.py
@@ -0,0 +1,6 @@
+# Рабочий бот SOMIO POLICE
+BOT_TOKEN = '6629159570:AAHo1KKdW-AaofcUpw-1GjHBeK1j3EYWpsM'
+# Рабочий чат SOMIO
+GROUP_ID = '-1001452837388'
+
+SUPERUSERS = ['sdg9999', 'Nikita_SOMIO22', 'hardcorno', 'OlegBKzn']
