071b12c8db76db2ff00a613833d5cc1c9740f7ac
Sergey Dmitrievich <129999088+S-D-Gorin@users.noreply.github.com>
2024-03-18 12:08:31 +0300

    dsfs

--- config.py
+++ config.py
@@ -7,27 +7,18 @@ import os
 DB_PATH = './databases/'
 
 
-DEBUG = os.getenv('DEBUG_MODE')
-# DEBUG = False
-
-if DEBUG == 'y':
-    # My test bot
-    BOT_TOKEN = os.getenv('TEST_BOT_TOKEN')
-else:
-    # ChatPolice bot
-    BOT_TOKEN = os.getenv('BOT_TOKEN')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 
 # My chats
 GROUP_ID = (
-    # 'None',
     '-1001901700124',  # Test Chat
     '-1001647209747',  # SOMIO
     '-1001928504949',  # Monolit
     ) 
 
 SUPERUSERS = (
-    # 'sdg9999',
+    'sdg9999',
     'Nikita_SOMIO22',
     'hardcorno', 
     'OlegBKzn',
